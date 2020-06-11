f814cdd84c353f7ff62c4b35335127baada47d65
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-06-11 10:00:47 +1000

    Add index for card files

---
+++ Components/index.js
@@ -0,0 +1,21 @@
+import ProductCard from "./ProductCard";
+import TestimonialCard from "./TestimonialCard";
+import CateringCard from "./CateringCard";
+import SpecialCard from "./SpecialCard";
+import GalleryCard from "./GalleryCard";
+import EventsCard from "./EventsCard";
+import AboutCard from "./AboutCard";
+import ContactCard from "./ContactCard";
+import MenuCard from "./MenuCard";
+
+export {
+  ProductCard,
+  TestimonialCard,
+  CateringCard,
+  SpecialCard,
+  GalleryCard,
+  EventsCard,
+  AboutCard,
+  ContactCard,
+  MenuCard,
+};
