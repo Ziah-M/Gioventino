b9aa7bcf61bd06d2751ed3c7561c60bca800ca17
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-06-12 16:36:18 +1000

    Extracted Dark Overlay

--- Themes/DarkTheme.js
+++ Themes/DarkTheme.js
@@ -1,35 +1,28 @@
-import styled, { css } from "styled-components";
-
 import React from "react";
+import styled, { css } from "styled-components";
+import DarkOverlay from "./DarkOverlay";
 
 const texture = `https://images.pexels.com/photos/966927/pexels-photo-966927.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
 
 const ThemedContainer = props => {
+  const height = props.height || "100vh"; // Optional height property
   return (
     <BgContainer {...props}>
-      <DarkOverlay>{props.children}</DarkOverlay>
+      <DarkOverlay transparency={0.8} height={height}>
+        {props.children}
+      </DarkOverlay>
     </BgContainer>
   );
 };
 
 export default ThemedContainer;
 
-const DarkOverlay = styled.div`
-  background: rgba(0, 0, 0, 0.8);
-  background-size: cover;
-  background-position: center center;
-  background-attachment: fixed;
-  width: 100vw;
-  margin: 0;
-  padding: 0;
-  z-index: 2;
-`;
-
 const BgContainer = styled.div`
   background-size: cover;
   background-position: center center;
   background-attachment: fixed;
   width: 100vw;
+  height: auto;
   margin: 0;
   padding: 0;
   background-image: url(${texture});
