d154e403a98c06eb5403cf7e54fb34c51ada585f
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-06-12 15:16:58 +1000

    Input Icon & Input Styling

--- Pages/Footer/index.js
+++ Pages/Footer/index.js
@@ -10,6 +10,8 @@ import {
   FormControl
 } from "react-bootstrap";
 import SocialButtons from "../../Components/SocialButtons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
@@ -53,7 +55,9 @@ const Footer = () => {
               <InputGroup>
                 <Control placeholder="Email" />
                 <InputGroup.Prepend>
-                  <InputIcon>@</InputIcon>
+                  <InputIcon>
+                    <FontAwesomeIcon icon={faLocationArrow} />
+                  </InputIcon>
                 </InputGroup.Prepend>
               </InputGroup>
             </Col>
@@ -75,19 +79,31 @@ const Footer = () => {
 };
 
 const InputGroup = styled(UnstyledInputGroup)`
-  border: 1px solid white;
+  border: 1px solid #a8a8a8 !important;
+  outline: none !important;
   width: 50vw;
   max-width: 500px;
 `;
 
 const Control = styled(FormControl)`
-  color: black;
-  background: transparent;
+  color: #a8a8a8 !important;
+  background-color: transparent !important;
+  border: none !important;
+  outline: none !important;
+  &::placeholder {
+    color: #a8a8a8 !important;
+  }
 `;
 
 const InputIcon = styled(InputGroup.Text)`
   color: black;
   background-color: #deb150;
+  border: none !important;
+
+  &:hover {
+    color: #deb150;
+    background-color: black;
+  }
 `;
 
 const Row = styled(UnstyledRow)``;
