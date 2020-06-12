cefb3a767b1c114868e84f4876c99bc881f84640
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-06-12 15:56:43 +1000

    Change button style for ButtonAlt

--- Components/Buttons/StyledButton.js
+++ Components/Buttons/StyledButton.js
@@ -1,37 +1,39 @@
-import { Button } from 'react-bootstrap'
-import styled, {css} from 'styled-components'
+import { Button } from "react-bootstrap";
+import styled, { css } from "styled-components";
+
+// TODO --- get hover styles from ProductCard
+// TODO --- create a LIGHT version and a DARK version
 
 const StyledButton = styled(Button)`
-    ${props => 
-    props.light ? css`
-    border: 2px solid ${props => props.theme.primaryAccent};
-    color: ${props => props.theme.primaryAccent};
-    background: rgba(255,255,255,0);
+  ${props =>
+    props.light
+      ? css`
+          border: 2px solid ${props => props.theme.primaryAccent};
+          color: ${props => props.theme.primaryAccent};
+          background: rgba(255, 255, 255, 0);
 
-    &:hover{
-        background: ${props => props.theme.primaryAccent};
-        color:white;
-    }
-    ` 
-    : css`
-    border: 1px solid ${props => props.theme.primaryAccent};
-    color: ${props => props.theme.primaryAccent};
-    background: rgba(255,255,255,0);
+          &:hover {
+            background: ${props => props.theme.primaryAccent};
+            color: white;
+          }
+        `
+      : css`
+          border: 1px solid ${props => props.theme.primaryAccent};
+          color: ${props => props.textHover || props.theme.primaryAccent};
+          background: rgba(255, 255, 255, 0);
 
-    &:hover{
-        background: ${props => props.theme.primaryAccent};
-        color:black !important;
-        border:2px solid ${props => props.theme.primaryAccent} !important;
-        background-color: ${props => props.theme.primaryAccent};
-        ${css`
-        transition: 400ms ease-in-out background;
-        transition: 400ms ease color;
-      `}
-    }
-    ` 
-    }
-
-`
+          &:hover {
+            background: ${props => props.theme.primaryAccent};
+            color: black !important;
+            border: 2px solid ${props => props.theme.primaryAccent} !important;
+            background-color: ${props => props.theme.primaryAccent};
+            ${css`
+              transition: 400ms ease-in-out background;
+              transition: 400ms ease color;
+            `}
+          }
+        `}
+`;
 
 const ButtonDark = styled(StyledButton)`
   background: ${props => props.theme.dark};
@@ -39,22 +41,18 @@ const ButtonDark = styled(StyledButton)`
 `;
 
 const ButtonAlt = styled(StyledButton)`
-border: 2px solid black;
-color: ${props => props.theme.lightText};
-&:hover {
-  color: white !important;
-  background-color: black;
-  ${css`
-    transition: 400ms ease-in-out background;
-    transition: 400ms ease color;
-  `}
-}
+  border: 2px solid black;
+  color: ${props => props.theme.lightText};
+  &:hover {
+    color: ${props => (props.textHover ? props.textHover : "white !important")};
+    background-color: black;
+    ${css`
+      transition: 400ms ease-in-out background;
+      transition: 400ms ease color;
+    `}
+  }
 `;
 
-export default StyledButton
+export default StyledButton;
 
-export {
-    ButtonDark,
-    StyledButton,
-    ButtonAlt,
-}
+export { ButtonDark, StyledButton, ButtonAlt };
