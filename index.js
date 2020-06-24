9da3ca0c0b39f749c1adced83ab07ecf82642c55
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-06-25 09:38:54 +1000

    Various minor alterations

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
 
-export default App
+//CSS DEPENDENCIES
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
