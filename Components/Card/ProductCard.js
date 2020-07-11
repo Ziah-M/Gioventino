500144adbe86cb37b05c09d019db205f899bf9fb
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-07-12 03:27:56 +1000

    Responsiveness fixes

--- Components/Card/ProductCard.js
+++ Components/Card/ProductCard.js
@@ -13,67 +13,81 @@ const ProductCard = props => {
           <b>{price}</b>
         </PriceOverlay>
 
-        <Row>
-          <ColCentered xs={12}>
-            <StyledSubtitle>
-              {title}
-              <br />
-              ~~~~~~~
-            </StyledSubtitle>
-          </ColCentered>
+        <InnerContainer fluid>
+          <Row noGutters>
+            <ColCentered xs={12}>
+              <StyledSubtitle>
+                {title}
+                <br />
+                ~~~~~~~
+              </StyledSubtitle>
+            </ColCentered>
+          </Row>
 
-          <ColCentered xs={12}>
-            <StyledText>
-              <Row noGutters>
-                <Col xs={12} style={{ textAlign: "center" }}>
-                  {text
-                    .split(",")
-                    .map((word, index) =>
-                      index === 0 || index === 1 ? word + "," : null
-                    )}
-                </Col>
-              </Row>
-              <Row noGutters>
-                <Col xs={12} style={{ textAlign: "center" }}>
-                  {text
-                    .split(",")
-                    .map((word, index) =>
-                      index === 2 || index === 3 ? word + "," : null
-                    )}
-                </Col>
-              </Row>
-              <Row noGutters>
-                <Col xs={12} style={{ textAlign: "center" }}>
-                  {text
-                    .split(",")
-                    .map((word, index) =>
-                      index === 4 || index === 5 ? word + "," : null
-                    )}
-                </Col>
-              </Row>
-              <Row noGutters>
-                <Col xs={12} style={{ textAlign: "center" }}>
-                  {text
-                    .split(",")
-                    .map((word, index) =>
-                      index === 6 || index === 7 ? word + "," : null
-                    )}
-                </Col>
-              </Row>
-            </StyledText>
-          </ColCentered>
+          <Row>
+            <ColCentered xs={12}>
+              <StyledText>
+                <Row noGutters>
+                  <Col xs={12} style={{ textAlign: "center" }}>
+                    {text
+                      .split(",")
+                      .map((word, index) =>
+                        index === 0 || index === 1 ? word + "," : null
+                      )}
+                  </Col>
+                </Row>
+                <Row noGutters>
+                  <Col xs={12} style={{ textAlign: "center" }}>
+                    {text
+                      .split(",")
+                      .map((word, index) =>
+                        index === 2 || index === 3 ? word + "," : null
+                      )}
+                  </Col>
+                </Row>
+                <Row noGutters>
+                  <Col xs={12} style={{ textAlign: "center" }}>
+                    {text
+                      .split(",")
+                      .map((word, index) =>
+                        index === 4 || index === 5 ? word + "," : null
+                      )}
+                  </Col>
+                </Row>
+                <Row noGutters>
+                  <Col xs={12} style={{ textAlign: "center" }}>
+                    {text
+                      .split(",")
+                      .map((word, index) =>
+                        index === 6 || index === 7 ? word + "," : null
+                      )}
+                  </Col>
+                </Row>
+              </StyledText>
+            </ColCentered>
+          </Row>
 
-          <ColCentered xs={12}>
-            <StyledButton>
-              <b>ORDER NOW -></b>
-            </StyledButton>
-          </ColCentered>
-        </Row>
+          <Row>
+            <ColCentered xs={12}>
+              <StyledButton>
+                <b>ORDER NOW -></b>
+              </StyledButton>
+            </ColCentered>
+          </Row>
+        </InnerContainer>
       </StyledCardBody>
       <StyledBorder />
     </StyledCard>
   );
 };
+
+const InnerContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px 0;
+  height: 100%;
+`;
 
 // THIS ELEMENT IS PURELY STYLISTIC
 const StyledBorder = styled.div`
@@ -86,8 +100,8 @@ const StyledBorder = styled.div`
 `;
 
 const ColCentered = styled(Col)`
-  display: grid;
-  justify-items: center;
+  display: flex;
+  justify-content: center;
 `;
 
 // BOOTSTRAP STYLES
@@ -129,8 +143,10 @@ const StyledButton = styled(Button)`
   border: 2px solid black;
   color: black;
   padding: 2% 4%;
-  margin: 0 0 5% 0;
-  font-size: 80%;
+  font-size: 70%;
+  width: 120px;
+  min-width: 60%;
+  min-height: 45px;
 
   &:hover {
     color: white !important;
@@ -147,12 +163,16 @@ const PriceOverlay = styled.div`
   position: absolute;
   color: white;
   background-color: black;
-  min-width: 35px;
-  width: 5%;
+  min-width: 50px;
+  width: 10%;
   height: 5%;
   min-height: 25px;
   text-align: center;
-  top: -9%;
+  top: -6%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.8;
 `;
 
 export default ProductCard;
